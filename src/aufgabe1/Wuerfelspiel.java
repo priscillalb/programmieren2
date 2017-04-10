92c4c0df0d981c141f0e162195171757a7304027
Priscilla <s0548696@htw-berlin.de>
2017-04-10 19:35:59 +0200

    Initial commit

---
+++ src/aufgabe1/Wuerfelspiel.java
@@ -0,0 +1,19 @@
+package aufgabe1;
+import javax.swing.JOptionPane;
+
+/**
+ * Created by Priscilla on 10.04.2017.
+ */
+public class Wuerfelspiel {
+
+    public static void main(String[] args){
+
+     int eingabe = JOptionPane.showConfirmDialog(null, "Geben sie ihr EInverstandnis", "Einverstandnis",JOptionPane.YES_NO_OPTION);
+
+
+    System.out.println(eingabe);
+
+
+    }
+
+}
